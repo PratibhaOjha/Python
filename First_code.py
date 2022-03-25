817c58bacd2e86d28b7a8ea07d7ea63c346af1ed
Pratibha <mepratibha97@gmail.com>
2022-03-25 13:34:54 +0530

    First commit

---
+++ First_code.py
@@ -0,0 +1,6 @@
+a= (1,6,3,51,15)
+print(a)
+# 1) a.count(value)  : returns the no. of times the value occurs in a
+print(a.count(3))
+# 2) a.index(value) : returns the index of first occurence of value in a
+print(a.index(51))
